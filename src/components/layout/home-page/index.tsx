09e40e5a5d09603ceda7a3d955966bd4ac9ce60c
Faisal Jamil <jamilfaisal43@gmail.com>
2025-08-20 21:19:23 -0400

    refactor: Flatten subfolders with only one tsx file

--- src/components/layout/home-page/index.tsx
+++ src/components/layout/home-page/index.tsx
@@ -2,9 +2,9 @@
 
 import { WeddingInfo } from '../types';
 import CeremonyInfo from './ceremony-info/ceremony-info';
-import EventTimeline from './event-timeline/event-timeline';
+import EventTimeline from './event-timeline';
 import HeroSection from './hero-section/hero-section';
-import WeddingDetails from './wedding-details/wedding-details';
+import WeddingDetails from './wedding-details';
 
 function HomePage({ weddingInfo }: { weddingInfo: WeddingInfo }) {
   return (
