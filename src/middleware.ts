9edb3bfd7562c0d4e06570e215ee09b841d9edcd
Faisal Jamil <jamilfaisal43@gmail.com>
2025-09-01 17:21:34 -0400

    Update security headers in middleware; remove unnecessary Unsplash domain from img-src directive

--- src/middleware.ts
+++ src/middleware.ts
@@ -150,7 +150,7 @@ function addSecurityHeaders(response: NextResponse): NextResponse {
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://images.unsplash.com https://unsplash.com;
+    img-src 'self' blob: data: https://images.unsplash.com;
     font-src 'self' https://fonts.gstatic.com;
     object-src 'none';
     base-uri 'self';
