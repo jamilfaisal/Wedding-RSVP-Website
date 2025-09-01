c522a9df6b4fbbd49e168ac7c14c03c8c412f45e
Faisal Jamil <jamilfaisal43@gmail.com>
2025-09-01 18:19:02 -0400

    Add type declarations for @public image imports to fix build errors

---
+++ src/types/image-imports.d.ts
@@ -0,0 +1,50 @@
+// Type declarations for image imports using @public alias
+declare module '@public/images/*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/hotels/*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/hotels/*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/hotels/*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '@public/images/hotels/*.webp' {
+  const src: string;
+  export default src;
+}
